881f2edc4727c44ea7ffffaf1f0a5d3b4a5192fe
Shankk <brianviveiros@hotmail.com>
2023-08-03 07:01:39 -0400

    added find, insert, remove. remove method is not done for all scenarios.

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const Tree = require('./binary-search-tree')
 
-const sorted = [1,2,3,4,5,6,7]
+const sorted = [1,2,3,4,5,6,7,13,14,15,18,20]
 const bst = new Tree(sorted)
+bst.tree = bst.buildTree(bst.array, 0, bst.array.length-1)
 
-console.log(bst.buildTree(bst.array))
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.rightChild !== null) {
+      prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.root}`);
+    if (node.leftChild !== null) {
+      prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
+prettyPrint(bst.tree)
+//console.log(bst.find(9))
+//bst.remove(7)
+prettyPrint(bst.tree)
+//console.log(bst.locateLeaf(8))
--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,17 +1,87 @@
 class Tree {
     constructor(array = []) {
         this.array = array
+        this.tree = null
     }
 
-    buildTree(array = [], start = 0, end = (array.length-1)) {
+    buildTree(array = [], start, end) {
         if(start>end) return null
-        let mid = (start+end)/2
+        let mid = Math.floor((start+end)/2)
         let root = new Node(array[mid]) 
 
         root.setLeft(this.buildTree(array,start,mid-1))
         root.setRight(this.buildTree(array,mid+1,end))
 
         return root
+    }
+
+    find(value) {
+        if(this.tree === null) return null
+        let node = this.tree
+        while(value != null) {
+            if(value > node.root && node.rightChild != null) {
+                node = node.rightChild
+             
+            }
+            else if(value < node.root && node.leftChild != null) {
+                node = node.leftChild
+            }
+            else if(value == node.root){
+                return node
+            }
+            else {
+                return null
+            }
+        }
+    }
+
+    locateLeaf(value) {
+        if(this.tree === null) return null
+        let node = this.tree
+        while(value != null) {
+            if(value > node.root && node.rightChild != null) {
+                node = node.rightChild
+            }
+            else if(value < node.root && node.leftChild != null) {
+                node = node.leftChild
+            }
+            else if(value == node.root){
+                return null
+            }
+            else {
+                return node
+            }
+        }
+    }
+
+    insert(value) {
+        let item = this.locateLeaf(value)
+        if(item == null) return null
+        if(value > item.root) item.rightChild = new Node(value)
+        if(value < item.root) item.leftChild = new Node(value)
+    }
+
+    remove(value) {
+        let item = this.find(value)
+        if(item == null) return null
+        // FIX THIS: has both children, we search for next best to take over
+        if(item.rightChild != null && item.leftChild != null){
+            
+        }
+        // only has rightchild, we replace it with rightchild
+        else if(item.rightChild != null && item.leftChild == null) {
+            item.root = item.rightChild.root
+            item.rightChild = null
+        }
+        // only has leftchild, we replace it with left child
+        else if(item.rightChild == null && item.leftChild != null) { 
+            item.root = item.leftChild.root
+            item.leftChild = null
+        }
+        // if no children, we delete it
+        else { 
+            return item = null
+        }
     }
 } 
 
