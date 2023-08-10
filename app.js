3a0ccbea61e7590ffc3db17dfe5318c81e563207
Shankk <brianviveiros@hotmail.com>
2023-08-10 06:14:14 -0400

    added levelOrder and depth methods. added a basic drive to app.js that showcases the bst in the console

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Tree = require('./binary-search-tree')
 
-const myArray = [23,14,6,8,68,56,43,12,12,68]
+const myArray = [23,14,6,8,56,43,12,12,68]
 const bst = new Tree(myArray)
 //bst.tree = bst.buildTree(bst.array, 0, bst.array.length-1)
 
@@ -11,10 +11,29 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node.rightChild !== null) {
     prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
   }
-  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.root}`);
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
   if (node.leftChild !== null) {
     prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
   }
 };
 
+
 prettyPrint(bst.tree)
+bst.isBalanced(bst.tree)
+console.log(bst.levelOrder(bst.tree))
+console.log(bst.preorder(bst.tree))
+console.log(bst.postorder(bst.tree))
+console.log(bst.inorder(bst.tree))
+bst.insert(47)
+bst.insert(26)
+bst.insert(35)
+bst.insert(52)
+prettyPrint(bst.tree)
+bst.isBalanced(bst.tree)
+bst.rebalance()
+prettyPrint(bst.tree)
+bst.isBalanced(bst.tree)
+console.log(bst.levelOrder(bst.tree))
+console.log(bst.preorder(bst.tree))
+console.log(bst.postorder(bst.tree))
+console.log(bst.inorder(bst.tree))
--- binary-search-tree.js
+++ binary-search-tree.js
@@ -33,10 +33,39 @@ class Tree {
         return lh > rh ? lh + 1 : rh + 1        
     }
 
+    depth(node, root = this.tree ,level = 0) {
+        if(node == null || this.tree == null) return
+        if(root.value == node.value) return level
+        
+        if(node.value < root.value) {
+            return this.depth(node, root.leftChild, level+1)
+        }
+        else {
+            return this.depth(node, root.rightChild, level+1)
+        }
+        
+    }
+
+    levelOrder(node, array = []) {
+        if(node == null) return
+        let queue = []
+        queue.push(node)
+        while(queue.length > 0) {
+            let current = queue[0]
+            queue.shift()
+            array.push(current.value)
+            if(current.leftChild != null) queue.push(current.leftChild)
+            if(current.rightChild != null) queue.push(current.rightChild)
+
+        }
+
+        return array
+    }
+
     inorder(node, array = []) {
         if (node !== null) {
             this.inorder(node.leftChild, array);
-            array.push(node.root);
+            array.push(node.value);
             this.inorder(node.rightChild, array);
         }
 
@@ -44,7 +73,7 @@ class Tree {
     }
     preorder(node, array = []) {
         if (node !== null) {
-            array.push(node.root);
+            array.push(node.value);
             this.inorder(node.leftChild, array);
             this.inorder(node.rightChild, array);
         }
@@ -55,7 +84,7 @@ class Tree {
         if (node !== null) {
             this.inorder(node.leftChild, array);
             this.inorder(node.rightChild, array);
-            array.push(node.root);
+            array.push(node.value);
         }
         
         return array
@@ -76,14 +105,14 @@ class Tree {
         if(this.tree === null) return null
         let node = this.tree
         while(value != null) {
-            if(value > node.root && node.rightChild != null) {
+            if(value > node.value && node.rightChild != null) {
                 node = node.rightChild
              
             }
-            else if(value < node.root && node.leftChild != null) {
+            else if(value < node.value && node.leftChild != null) {
                 node = node.leftChild
             }
-            else if(value == node.root){
+            else if(value == node.value){
                 return node
             }
             else {
@@ -96,13 +125,13 @@ class Tree {
         if(this.tree === null) return null
         let node = this.tree
         while(value != null) {
-            if(value > node.root && node.rightChild != null) {
+            if(value > node.value && node.rightChild != null) {
                 node = node.rightChild
             }
-            else if(value < node.root && node.leftChild != null) {
+            else if(value < node.value && node.leftChild != null) {
                 node = node.leftChild
             }
-            else if(value == node.root){
+            else if(value == node.value){
                 return null
             }
             else {
@@ -114,8 +143,8 @@ class Tree {
     insert(value) {
         let item = this.locateLeaf(value)
         if(item == null) return null
-        if(value > item.root) item.rightChild = new Node(value)
-        if(value < item.root) item.leftChild = new Node(value)
+        if(value > item.value) item.rightChild = new Node(value)
+        if(value < item.value) item.leftChild = new Node(value)
     }
 
     remove(value) {
@@ -127,12 +156,12 @@ class Tree {
         }
         // only has rightchild, we replace it with rightchild
         else if(item.leftChild == null) {
-            item.root = item.rightChild.root
+            item.value = item.rightChild.value
             item.rightChild = null
         }
         // only has leftchild, we replace it with left child
         else if(item.rightChild == null) { 
-            item.root = item.leftChild.root
+            item.value = item.leftChild.value
             item.leftChild = null
         }
         //has both children, we search for next best to take over
@@ -153,21 +182,21 @@ class Tree {
                 succParent.rightChild = succ.rightChild;
             }
             // Copy Successor Data to root
-            item.root = succ.root;
+            item.value = succ.value;
             return item;
         }
     }
 } 
 
 class Node {
-    constructor(root = null ,leftChild = null,rightChild = null) {
-        this.root = root
+    constructor(value = null ,leftChild = null,rightChild = null) {
+        this.value = value
         this.leftChild = leftChild
         this.rightChild = rightChild
     }
 
-    setRoot(value) {
-        this.root = value
+    setValue(value) {
+        this.value = value
     }
 
     setLeft(value) {
